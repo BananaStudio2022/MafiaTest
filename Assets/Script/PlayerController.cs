412092714445de47a07a057a3d6703b5492b2350
BananaStudio2022 <104382244+BananaStudio2022@users.noreply.github.com>
2022-04-26 01:20:30 +0300

    6 commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = -1f;
+
+    private Vector2 _direction;
+    private Rigidbody2D _rb;
+
+    private float _horizontal = 0f;
+
+    const float speedXMultiplier = 50f;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _direction.x = Input.GetAxisRaw("Horizontal");
+        _direction.y = Input.GetAxisRaw("Vertical");
+        _rb.MovePosition(_rb.position + _direction * speed * Time.fixedDeltaTime);
+    }
+}
